b012c7a764b50bc9acc3020a713d2679011678b0
Sebastian <durhams199@potsdam.edu>
2019-03-01 01:33:45 -0500

    Wrote tests for models

--- autograder/personal/tests/test_models.py
+++ autograder/personal/tests/test_models.py
@@ -1,3 +1,144 @@
 from django.test import TestCase
 
 # Create your tests here.
+from personal.models import *
+
+class InviteModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Courses.objects.create(course_id = 1, course_title = 'Software Engineering')
+        Invite.objects.create(invite_id = 1, course_id = Courses.objects.get(course_id=1), 
+                              sender_username = 'durhams199',
+                              rec_username = 'rwilliam122')
+
+    def test_sender_username_label(self):
+        invite = Invite.objects.get(invite_id = 1)
+        field_label = invite._meta.get_field('sender_username').verbose_name
+        self.assertEquals(field_label, 'sender_username')
+
+    def test_rec_username_label(self):
+        invite = Invite.objects.get(invite_id = 1)
+        field_label = invite._meta.get_field('rec_username').verbose_name
+        self.assertEquals(field_label, 'rec_username')
+
+    def test_level_of_invite_label(self):
+        invite = Invite.objects.get(invite_id = 1)
+        field_label = invite._meta.get_field('level_of_invite').verbose_name
+        self.assertEquals(field_label, 'level_of_invite')
+
+    def test_sender_username_max_length(self):
+        invite = Invite.objects.get(invite_id = 1)
+        max_length = invite._meta.get_field('sender_username').max_length
+        self.assertEquals(max_length, 20)
+
+    def test_rec_username_max_length(self):
+        invite = Invite.objects.get(invite_id = 1)
+        max_length = invite._meta.get_field('rec_username').max_length
+        self.assertEquals(max_length, 20)
+
+    def test_level_of_invite_max_length(self):
+        invite = Invite.objects.get(invite_id = 1)
+        max_length = invite._meta.get_field('level_of_invite').max_length
+        self.assertEquals(max_length, 20)
+
+    def test_course_id_references_class(self):
+        invite = Invite.objects.get(invite_id=1)
+        course_test = Courses.objects.get(course_id=1)
+        self.assertEquals(invite.course_id, course_test)
+
+class CoursesModelTest(TestCase):
+    # Set up non-modified objects used by all test methods
+    @classmethod
+    def setUpTestData(cls):    
+        Courses.objects.create(course_id = 1, course_title = 'Software Engineering')
+
+    def test_course_title_label(self):
+        course_test = Courses.objects.get(invite_id = 1)
+        field_label = course_test._meta.get_field('course_title').verbose_name
+        self.assertEquals(field_label, 'course_title')
+
+    def test_instructor_username_label(self):
+        course_test = Courses.objects.get(invite_id = 1)
+        field_label = course_test._meta.get_field('instructor_username').verbose_name
+        self.assertEquals(field_label, 'instructor_username')
+
+    def test_course_title_max_length(self):
+        course = Courses.objects.get(invite_id = 1)
+        max_length = course._meta.get_field('course_title').max_length
+        self.assertEquals(max_length, 30)  
+
+    def test_instructor_username_max_length(self):
+        course = Courses.objects.get(invite_id = 1)
+        max_length = course._meta.get_field('instructor_username').max_length
+        self.assertEquals(max_length, 20)      
+
+class AssignmentModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):        
+        # Set up non-modified objects used by all test methods
+        Courses.objects.create(course_id = 1, course_title = 'Software Engineering')
+        Assignment.objects.create(assignment_id = 1, course_id = Courses.objects.get(course_id=1))
+
+    def test_assignment_description_label(self):
+        assignment_test = Assignment.objects.get(invite_id = 1)
+        field_label = assignment_test._meta.get_field('assignment_description').verbose_name
+        self.assertEquals(field_label, 'assignment_description')
+
+    def test_optional_notes_label(self):
+        assignment_test = Assignment.objects.get(invite_id = 1)
+        field_label = assignment_test._meta.get_field('optional_notes').verbose_name
+        self.assertEquals(field_label, 'optional_notes')
+
+    def test_acceptance_criteria_label(self):
+        assignment_test = Assignment.objects.get(invite_id = 1)
+        field_label = assignment_test._meta.get_field('acceptance_criteria').verbose_name
+        self.assertEquals(field_label, 'acceptance_criteria')
+
+    def test_testing_criteria_label(self):
+        assignment_test = Assignment.objects.get(invite_id = 1)
+        field_label = assignment_test._meta.get_field('testing_criteria').verbose_name
+        self.assertEquals(field_label, 'testing_criteria')
+
+    def test_assignment_description_max_length(self):
+        assignment_test = Assignment.objects.get(invite_id = 1)
+        max_length = assignment_test._meta.get_field('assignment_test').max_length
+        self.assertEquals(max_length, 200)
+
+    def test_optional_notes_max_length(self):
+        assignment_test = Assignment.objects.get(invite_id = 1)
+        max_length = assignment_test._meta.get_field('optional_notes').max_length
+        self.assertEquals(max_length, 200)
+
+    def test_acceptance_criteria_max_length(self):
+        assignment_test = Assignment.objects.get(invite_id = 1)
+        max_length = assignment_test._meta.get_field('acceptance_criteria').max_length
+        self.assertEquals(max_length, 200)  
+
+    def test_testing_criteria_max_length(self):
+        assignment_test = Assignment.objects.get(invite_id = 1)
+        max_length = assignment_test._meta.get_field('testing_criteria').max_length
+        self.assertEquals(max_length, 200)  
+
+    def test_course_id_references_class(self):
+        assignment_test = Assignment.objects.get(invite_id=1)
+        course_test = Courses.objects.get(course_id=1)
+        self.assertEquals(assignment_test.course_id, course_test)
+
+class SubmissionModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):        
+        # Set up non-modified objects used by all test methods
+        Course.objects.create(course_id = 1, course_title = 'Software Engineering')
+        Assignment.objects.create(assignment_id = 1, course_id = Courses.objects.get(course_id=1))
+        Submission.objects.create(submission_id = 1, assignment_id = Assignment.objects.get(assignment_id=1))
+
+    def test_submitted_user_label(self):
+        submission_test = Submission.objects.get(invite_id = 1)
+        field_label = submission_test._meta.get_field('submitted_user').verbose_name
+        self.assertEquals(field_label, 'submitted_user')
+
+    def test_testing_criteria_max_length(self):
+        submission_test = Assignment.objects.get(invite_id = 1)
+        max_length = submission_test._meta.get_field('submitted_user').max_length
+        self.assertEquals(max_length, 20)  
