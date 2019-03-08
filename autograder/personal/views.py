7bfcc135d28a2168b0fc2ff645bb027fa399c695
malonejc200 <malonejc200@potsdam.edu>
2019-03-08 01:41:10 +0000

    Added a home page that shows a list of courses

--- autograder/personal/views.py
+++ autograder/personal/views.py
@@ -5,5 +5,8 @@ from django.shortcuts import render
 def index(request) :
     return render(request, 'personal/home.html')
 
+def homepage(request) :
+    return render(request, 'personal/homepage.html')
+
 def contact(request) :
     return render(request, 'personal/basic.html',{'content':['If you would like to contact me, please email me.','moellej200@potsdam.edu']})
