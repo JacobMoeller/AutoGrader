e137d6fb9c91d6a924a74d366ce0eba96d24475b
Sebastian Durham <durhams199@potsdam.edu>
2019-03-28 18:19:11 -0400

    Changed models to use user table and changed tests to reflect changes

--- autograder/personal/models.py
+++ autograder/personal/models.py
@@ -1,44 +1,151 @@
 from django.db import models
-from datetime import timedelta
+from datetime import timedelta, date
 from django.utils import timezone
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+User = get_user_model()
+
+
 class Courses(models.Model):
-    course_id = models.IntegerField(primary_key=True)
-    course_title = models.CharField(max_length=30, blank=False, null=False)
-    instructor_username = models.CharField(max_length=20, blank=False, null=False)
+    course_crn = models.IntegerField(
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name="CRN",
+        validators=[MinValueValidator(1)])
+    course_title = models.CharField(max_length=50, blank=False, null=False)
+    course_number = models.IntegerField(
+        blank=False,
+        null=False,
+        validators=[MinValueValidator(1)])
+    instructor_username = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING,
+        limit_choices_to={'groups__name': "Instructor"},
+        blank=False,
+        null=False,
+        to_field='username')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Course"
+        ordering = ['course_number', ]
+
+    def __str__(self):
+        return f'{self.course_number} {self.course_title} ({self.course_crn})'
+
+    #def get_absolute_url(self):
+    #    return reverse('course_detail', kwargs={'pk': self.id})
+
 
 def one_month_from_today():
     return timezone.now() + timedelta(days=30)
 
+
 class Invite(models.Model):
-    invite_id = models.IntegerField(primary_key=True)
-    sender_username = models.CharField(max_length=20, blank=False, null=False)
-    rec_username = models.CharField(max_length=20, blank=False, null=False)
-    course_id = models.ForeignKey('Courses', models.DO_NOTHING, blank=False, null=False)
-    level_of_invite = models.CharField(max_length=10, blank=False, null=False)
+    sender_username = models.ForeignKey(
+            User,
+            on_delete=models.DO_NOTHING,
+            limit_choices_to={'groups__name': "Instructor"},
+            blank=False,
+            null=False,
+            to_field='username',
+            related_name='sender',
+            verbose_name='from')
+    rec_username = models.ForeignKey(
+            User,
+            on_delete=models.DO_NOTHING,
+            blank=False,
+            null=False,
+            to_field='username',
+            related_name='receiver',
+            verbose_name='to')
+    course_id = models.ForeignKey(
+        'Courses', on_delete=models.CASCADE, blank=False, null=False)
+    INVITE_TYPES = (
+        ('g', 'Grader'),
+        ('s', 'Student'),
+        ('i', 'Instructor'),
+    )
+    level_of_invite = models.CharField(
+        max_length=1, choices=INVITE_TYPES, default='s')
     created_at = models.DateTimeField(auto_now_add=True)
     expires_on = models.DateField(default=one_month_from_today, null=False)
 
+    def time_left(self):
+        remaining = self.expires_on - date.today()
+        return f'{remaining.days} day(s)'
+
+    class Meta:
+        ordering = ['-expires_on', 'sender_username', ]
+
+    def __str__(self):
+        return f'{self.get_level_of_invite_display()} Invite for {self.rec_username} ({self.course_id})'
+
+
 class Assignment(models.Model):
-    assignment_id = models.IntegerField(primary_key=True)
-    course_id = models.ForeignKey('Courses', models.DO_NOTHING, blank=False, null=False)
-    assignment_description = models.CharField(max_length=200, blank=False, null=False)
+    course_id = models.ForeignKey(
+        'Courses', on_delete=models.CASCADE, blank=False, null=False)
+    title = models.CharField(max_length=200)
+    assignment_description = models.CharField(
+        max_length=200, blank=False, null=False)
     optional_notes = models.CharField(max_length=200, blank=True, null=True)
-    acceptance_criteria = models.CharField(max_length=200, blank=False, null=False)
-    testing_criteria = models.CharField(max_length=200, blank=False, null=False)
+    acceptance_criteria = models.CharField(
+        max_length=200, blank=True, null=True)
+    testing_criteria = models.CharField(
+        max_length=200, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     due_on = models.DateTimeField(auto_now_add=False, null=True)
 
+    class Meta:
+        ordering = ['course_id', 'due_on', 'title', ]
+
+    def __str__(self):
+        return f'{self.title} (for {self.course_id.course_title})'
+
+
 class Submission(models.Model):
-    submission_id = models.IntegerField(primary_key=True)
-    assignment_id = models.ForeignKey('Assignment', models.DO_NOTHING, blank=False, null=False)
-    submitted_user = models.CharField(max_length=20, blank=False, null=False)
+    assignment_id = models.ForeignKey(
+        'Assignment', models.DO_NOTHING, blank=False, null=False)
+    submitted_user = models.ForeignKey(
+        User,
+        limit_choices_to={'groups__name': "Student"},
+        blank=False,
+        null=False,
+        on_delete=models.DO_NOTHING,
+        to_field='username')
     submitted_on = models.DateTimeField(auto_now_add=True)
     submission_number = models.IntegerField(default=1)
     submitted_filename = models.CharField(max_length=20, blank=True, null=True)
 
+    class Meta:
+        ordering = ['submitted_user', 'assignment_id', 'submitted_on', ]
+
+    def __str__(self):
+        return f'{self.submitted_user.username}: \
+        {self.assignment_id} -- Attempt: {self.submission_number}'
+
+
 class Takes(models.Model):
-    course_id = models.ForeignKey('Courses', models.DO_NOTHING, blank=False, null=False)
-    student_username = models.CharField(max_length=20, blank=False, null=False)
+    course_id = models.ForeignKey(
+        'Courses', models.DO_NOTHING, blank=False, null=False)
+    student_username = models.ForeignKey(
+        User,
+        limit_choices_to={'groups__name': "Student"},
+        blank=False,
+        null=False,
+        on_delete=models.DO_NOTHING,
+        to_field='username')
+    is_grader_in_course = models.BooleanField(default=False, null=True)
+
+    class Meta:
+        verbose_name_plural = "Takes"
+        ordering = ['course_id__course_number', ]
+
+    def __str__(self):
+        takes_record = f'CIS{self.course_id.course_number} - {self.student_username}'
+        if(self.is_grader_in_course):
+            takes_record += '(Grader)'
+        return takes_record
--- autograder/personal/tests/test_views.py
+++ autograder/personal/tests/test_views.py
@@ -1,47 +1,57 @@
 from django.test import TestCase
-
-# Create your tests here.
+from django.contrib.auth.models import User, Group
 from django.urls import reverse
-
 from personal.models import Courses
+from django.contrib.auth.models import Group, Permission
+
+
 class InstructorHomePageViewTest(TestCase):
+    #fixtures = ['initial_data.json', ]
+
     @classmethod
     def setUpTestData(cls):
-        '''
+
         # Create two users
-        test_user1 = User.objects.create_user(username='testuser1', password='password')
-        test_user2 = User.objects.create_user(username='testuser2', password='password')
-        
-        test_user1.save()
-        test_user2.save()
-        '''
-        
-        #Create 4 courses
+        cls.test_group1 = Group.objects.create(name='Instructor')
+        cls.test_group1 = Group.objects.create(name='Student')
+        cls.test_group1 = Group.objects.create(name='Grader')
+        # Create two users
+        cls.test_user1 = User.objects.create_user(
+            username='testuser1',
+            password='password')
+        cls.test_user2 = User.objects.create_user(
+            username='testuser2',
+            password='password')
+        cls.test_user1.save()
+
+        # Add test_user1 to Instructor group
+        group = Group.objects.get(name="Instructor")
+        group.user_set.add(cls.test_user1)
+
+        cls.test_user2.save()
+
+        # Add test_user2 to Student group
+        group = Group.objects.get(name="Student")
+        group.user_set.add(cls.test_user2)
+
+        # Create 4 courses
         number_of_courses = 4
         for course_id in range(number_of_courses):
-            Courses.objects.create(
+            cls.course = Courses.objects.create(
                 course_title = f'CIS_201 {course_id}',
+                course_number=405, id=course_id,
+                course_crn=12345+course_id,
+                instructor_username=cls.test_user1,
             )
+            cls.course.save()
 
-    #Tests to see if the url is correct
+
+    # Tests to see if the url is correct
     def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/home/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    #Tests to see if the url name is accessible
+    ## Tests to see if the url name is accessible
     def test_view_url_accessible_by_name(self):
-        response = self.client.get(reverse('homepage'))
-        self.assertEqual(response.status_code, 200)
-
-    #Tests to see if the correct template is being used
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('homepage'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'personal/homepage.html')
-
-    #Tests to see if the 4 courses are being shown
-    #def test_view_all_courses(self):
-     #   response = self.client.get(reverse('homepage'))
-      #  self.assertEqual(response.status_code, 200)
-       # self.assertTrue(len(response.context['course_list']) == 4)
-
+         response = self.client.get(reverse('homepage'), follow=True)
+         self.assertEqual(response.status_code, 200)
