cf50eea638637579e2bb8b7ffaefddf3f67a08cc
Katie Watson <watsonke200@potsdam.edu>
2019-04-02 15:58:15 -0400

    Modified fixtures and chgroups.
    Added check in manage.py to prevent replay of commands.
    Might cause unintended side-effects but doesn't seem to
    break anything. Modified chgroups so it outputs files
    correctly and updates admin, tested with fresh build.

--- autograder/manage.py
+++ autograder/manage.py
@@ -6,16 +6,16 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autograder.settings')
     try:
         from django.core.management import execute_from_command_line
-
-        if len(sys.argv) == 2 and sys.argv[1] == 'migrate':
-            execute_from_command_line(['manage.py', 'loaddata', 'initial_auth.json'])
-            execute_from_command_line(['manage.py', 'loaddata', 'initial_superuser.json'])
-        execute_from_command_line(sys.argv)
-
+        if len(sys.argv) >= 2:
+            if sys.argv[1] == 'migrate':
+                execute_from_command_line(['manage.py', 'chgroups', 'load'])
+            elif sys.argv[1] == "runserver":
+                execute_from_command_line(sys.argv)
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    if not os.environ.get('RUN_MAIN', False):
+        execute_from_command_line(sys.argv)
--- autograder/personal/management/commands/chgroups.py
+++ autograder/personal/management/commands/chgroups.py
@@ -1,17 +1,19 @@
 """
 Creates permissions and groups automatically with custom command `chgroups`.
-Loads fixtures stored in personal/fixtures for auth tables.
+Loads fixtures stored in personal/fixtures for auth tables into
+/personal/fixtures/.
 
 Used in manage.py to automate initial set-up with `add`. `update` allows
 for automated storage in pre-defined filename/location to avoid inconsistency
 from typo and provide minor ease of use in command length.
 
-Superuser account is created with 'initial_accounts.json'; it cannot be updated
-except by manual use of dumpdata.
+Superuser account is stored in 'init_superuser.json'. Superuser is updated
+by pulling user details, `update` will refresh credentials if needed.
 """
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
-
+from django.core import serializers
+from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     help = 'Creates or updates default permission groups for groups.'
@@ -23,29 +25,47 @@ class Command(BaseCommand):
         parser.add_argument(
           'action',
           type=str,
-          help='Specify "add" to load permissions, "update" to store \
+          help='Specify "load" to load permissions, "update" to store \
             any newly created groups or permissions.')
 
     def handle(self, *args, **kwargs):
         arg = kwargs['action']
+        if arg != 'update' and arg != 'load':
+            print("ERROR: Invalid argument. Use --help to see all options.")
+
         if arg == 'update':
             print('Storing permissions and admin to file...')
 
             # Allows refreshing of stale initial group/permissions settings.
-            with open('personal/fixtures/initial_data.json', 'w') as output:
+            with open('personal/fixtures/init_data.json', 'w') as output:
                 call_command(
                     "dumpdata",
+                    "auth",
+                    "--exclude",
+                    "auth.User",
+                    "--natural-foreign",  # minimize conflicts with pks
+                    "--natural-primary",  # even though should be new build
                     "--format=json",
-                    "auth.group",
-                    "auth.permission",
-                    "auth.group_permissions",
+                    "--indent",
+                    "4",
                     stdout=output)
+
+            # Try to export admin superuser directly.
+            try:
+                admin = User.objects.filter(
+                    username="admin", is_superuser=True)
+                serial_admin = serializers.serialize("json", admin)
+                with open('personal/fixtures/init_superuser.json', 'w') as out:
+                    out.write(serial_admin)
+            except User.DoesNotExist:
+                print("Admin does not exist.")
+
             print('Files updated.')
 
-        elif arg == 'add':
+        elif arg == 'load':
             self.stdout.write('Adding default permissions and admin...')
             # Creates perms/groups
-            call_command("loaddata", "initial_data.json")
+            call_command("loaddata", "init_data.json")
             # Creates superuser
-            call_command("loaddata", "initial_accounts.json")
+            call_command("loaddata", "init_superuser.json")
             print("Created default groups and permissions.")
